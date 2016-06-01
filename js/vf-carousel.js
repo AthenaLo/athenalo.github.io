f0de98211954c4a909a1ff8eefe0d59371d66152
Kairui <kairui@plethora.com>
2016-05-31 20:47:46 -0700

    Add autoscroll to slideshow

--- js/vf-carousel.js
+++ js/vf-carousel.js
@@ -2,12 +2,20 @@ $(document).ready(function(){
   $('.vf-solution-carousel').slick({
     arrows: false,
     dots: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
     infinite: true,
+    autoplay: true,
+    autoplaySpeed: 5000,
     slidesToShow: 1,
     customPaging: function(slider, i) {
       return '<i class="fa fa-circle dot" aria-hidden="true"></i>';
     }
-  });
+  }).on('afterChange', function(event, slick, currentSlide, nextSlide){
+    next_active = $('.slick-active').parent().next().children('.dot');
+    $('.dot').removeClass('slick-active');
+    next_active.addClass('slick-active');
+  }); 
 
   $('.vf-solution-carousel li').click(function() {
     // Clear all highlighted dots before adding new highlight
