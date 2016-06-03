d8f3648f8a2430de1455bc778a744f49ea1bc0e7
Kairui <kairui@plethora.com>
2016-06-03 09:53:24 -0500

    New revision of video feedback project page

--- js/vf-carousel.js
+++ js/vf-carousel.js
@@ -11,17 +11,11 @@ $(document).ready(function(){
     customPaging: function(slider, i) {
       return '<i class="fa fa-circle dot" aria-hidden="true"></i>';
     }
-  }).on('afterChange', function(event, slick, currentSlide, nextSlide){
-    last_dot = $('.slick-dots').children().last().children('.dot');
-    if ($('i.slick-active')[0] === last_dot[0]) {
-      // The current dot is the last dot
-      next_active = $('i.slick-active').parent().parent().children().first().children('.dot');
-    } else {
-      // The current dot is not the last dot
-      next_active = $('i.slick-active').parent().next().children('.dot');
-    }
+  }).on('afterChange', function(event, slick, newSlideIdx){
+    dotSelector = "ul.slick-dots li:eq(" + newSlideIdx + ")";
+    new_active = $(dotSelector).children('.dot');
     $('.dot').removeClass('slick-active');
-    next_active.addClass('slick-active');
+    new_active.addClass('slick-active');
   }); 
 
   $('.vf-solution-carousel li').click(function() {
